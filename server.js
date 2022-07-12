23c49377d142c4fd958567a7eda768be8633f5e9
chasechri <christensonchase451@gmail.com>
2022-07-12 14:48:32 -0500

    html for animals page connecting to server

--- server.js
+++ server.js
@@ -95,6 +95,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
+app.get("/animals", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/animals.html"));
+});
+
 function validateAnimal(animal) {
   if (!animal.name || typeof animal.name !== "string") {
     return false;
