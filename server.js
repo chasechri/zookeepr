33312a3a4be27bfd7659b7ee8c00425bbc1fa766
chasechri <christensonchase451@gmail.com>
2022-07-12 15:00:36 -0500

    get req

--- server.js
+++ server.js
@@ -99,6 +99,14 @@ app.get("/animals", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/animals.html"));
 });
 
+app.get("/zookeepers", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
 function validateAnimal(animal) {
   if (!animal.name || typeof animal.name !== "string") {
     return false;
